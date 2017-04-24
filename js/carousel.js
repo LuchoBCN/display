079107799801c1abafdf27fd1ec7af7c4a6016e6
Luis Soria <luismiguel.ca@gmail.com>
2017-04-24 09:33:26 +0200

    Add files via upload

--- js/carousel.js
+++ js/carousel.js
@@ -5,7 +5,8 @@ $(document).ready( function() {
 	var clickEvent = false;
 	$('#myCarousel').on('click', '.nav a', function() {
 			clickEvent = true;
-			$(this).parent().addClass('active');		
+			$(this).parent().addClass('active');
+            $('.nav li').removeClass('active');
 	}).on('slid.bs.carousel', function(e) {
 		if(!clickEvent) {
 			var count = $('.nav').children().length -1;
@@ -30,7 +31,8 @@ $(document).ready( function() {
 	var clickEvent = false;
 	$('#Somos-myCarousel').on('click', '.nav a', function() {
 			clickEvent = true;
-			$(this).parent().addClass('active');		
+			$(this).parent().addClass('active');
+            $('#Somos-myCarousel .nav li').removeClass('active');
 	}).on('slid.bs.carousel', function(e) {
 		if(!clickEvent) {
 			var count = $('.nav').children().length -1;
@@ -50,7 +52,8 @@ $(document).ready( function() {
 	var clickEvent = false;
 	$('#cuanti-myCarousel').on('click', '.nav a', function() {
 			clickEvent = true;
-			$(this).parent().addClass('active');		
+			$(this).parent().addClass('active');
+            $('#cuanti-myCarousel .nav li').removeClass('active');
 	}).on('slid.bs.carousel', function(e) {
 		if(!clickEvent) {
 			var count = $('.nav').children().length -1;
@@ -70,7 +73,8 @@ $(document).ready( function() {
     	var clickEvent = false;
 	$('#cuali-myCarousel').on('click', '.nav a', function() {
 			clickEvent = true;
-			$(this).parent().addClass('active');		
+			$(this).parent().addClass('active');
+            $('#cuali-myCarousel .nav li').removeClass('active');
 	}).on('slid.bs.carousel', function(e) {
 		if(!clickEvent) {
 			var count = $('.nav').children().length -1;
@@ -90,7 +94,8 @@ $(document).ready( function() {
         var clickEvent = false;
 	$('#panel-myCarousel').on('click', '.nav a', function() {
 			clickEvent = true;
-			$(this).parent().addClass('active');		
+			$(this).parent().addClass('active');
+            $('#panel-myCarousel .nav li').removeClass('active');
 	}).on('slid.bs.carousel', function(e) {
 		if(!clickEvent) {
 			var count = $('.nav').children().length -1;
